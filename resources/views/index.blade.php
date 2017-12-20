611eefb02f37b3d7f8495287248416f969433208
龔育晴 <ching85117@gmail.com>
2017-12-20 21:43:03 +0800

    建立 index 視圖，編輯畫面

---
+++ resources/views/index.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>85度c</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            width: 100%;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100%;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-top: 0px;
+
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @if (Auth::check())
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ url('/login') }}">Login</a>
+                <a href="{{ url('/register') }}">Register</a>
+            @endif
+        </div>
+    @endif
+
+    <div class="content">
+        <div class="title m-b-md">
+            <img src="https://upload.wikimedia.org/wikipedia/zh/c/cd/Logo_85%E5%BA%A6C.png" width="50%" height="50%">
+        </div>
+
+        <div class="links">
+            <a href="https://laravel.com/docs">商品</a>
+            <a href="https://laracasts.com">購物車</a>
+            <a href="https://laravel-news.com">會員專區</a>
+            <a href="https://forge.laravel.com">聯絡我們</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
