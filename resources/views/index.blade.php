fd03582bf7dcce0516837ce2b99bcff3b2f79436
龔育晴 <ching85117@gmail.com>
2017-12-30 23:37:05 +0800

    首頁使用套版，並加入照片輪播

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,104 +1,45 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>85度c</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            width: 100%;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100%;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-        .top-left {
-            position: absolute;
-            left: 10px;
-            top: 18px;
-        }
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-top: 0px;
-
-        }
-    </style>
-</head>
-<body>
-<div class="flex-center position-ref full-height">
-<div class="top-left links">
-    <img src="https://upload.wikimedia.org/wikipedia/zh/c/cd/Logo_85%E5%BA%A6C.png" width="50%" height="50%">
-</div>
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @if (Auth::check())
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ url('/login') }}">Login</a>
-                <a href="{{ url('/register') }}">Register</a>
-            @endif
-        </div>
-    @endif
+@extends('layouts.master')
+@section('title','首頁')
+@section('content')
 
 
-    <div class="content">
-        <div class="title m-b-md">
+<center>
+<div class="container">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" style="width: 960px; height:500px;">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="http://www.85cafe.com/file/banner_index/960x500/%E5%85%83%E6%97%A6%E6%96%B0%E5%B9%B4%E6%96%B0%E6%9B%99%E5%85%89-960X500.jpg" alt="event1" style="width:960px; height:500px;">
+            </div>
+
+            <div class="item">
+                <img src="http://www.85cafe.com/file/banner_index/960x500/20171204%E9%9B%AAQ%E9%A4%85_%E7%B6%B2%E9%A0%81960X500.jpg" alt="event2" style="width:960px; height:500px;">
+            </div>
+
+            <div class="item">
+                <img src="http://www.85cafe.com/file/banner_index/960x500/%E9%BB%91%E8%89%B2%E7%B9%BD%E7%B4%9B%E9%BA%B5%E5%8C%85%E5%B0%88%E6%A1%88%E7%B6%B2%E9%A0%81-960x500-01.jpg" alt="event3" style="width:100%;">
+            </div>
         </div>
 
-        <div class="links">
-            <a href="https://laravel.com/docs">商品</a>
-            <a href="https://laracasts.com">購物車</a>
-            <a href="https://laravel-news.com">會員專區</a>
-            <a href="https://forge.laravel.com">聯絡我們</a>
-        </div>
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 </div>
-</body>
-</html>
+</center>
+
+
+    @endsection
