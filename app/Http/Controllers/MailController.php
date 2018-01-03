3b7bae247aae986cf9a6e5a774866220362a4060
龔育晴 <ching85117@gmail.com>
2018-01-03 21:58:50 +0800

    建立 MailController

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+    public function getSend()
+    {
+       /* $from=;
+        $to=;
+        $content=;
+        $data=['name'=>] */
+    }
+}
